df5df261dbbc44d019ea01915cde24f0e59c786a
Alexander Moerman <alexander.moerman@gmail.com>
2022-06-16 14:14:10 +0200

    Add benchmarks

---
+++ KeyedSemaphores.Benchmarks/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Concurrent;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using KeyedSemaphores;
+
+BenchmarkRunner.Run<KeyedSemaphoreBenchmarks>();
+
+[MemoryDiagnoser]
+public class KeyedSemaphoreBenchmarks
+{
+    [Params(1, 10, 100)] 
+    public int NumberOfLocks { get; set; }
+
+    [Params(1, 10, 100)] 
+    public int Contention { get; set; }
+
+    [Benchmark(Baseline = true)]
+    public async Task DictionaryOfSemaphoreSlims()
+    {
+        var semaphores = new ConcurrentDictionary<string, SemaphoreSlim>(Environment.ProcessorCount, NumberOfLocks);
+        var tasks = Enumerable.Range(0, Contention * NumberOfLocks).Select(async i =>
+        {
+            string key = i.ToString();
+            SemaphoreSlim? semaphore;
+            
+            // get or create a semaphore
+            while (true)
+            {
+                if (semaphores.TryGetValue(key, out semaphore))
+                    break;
+
+                semaphore = new SemaphoreSlim(1, 1);
+
+                if (semaphores.TryAdd(key, semaphore))
+                    break;
+                
+                semaphore.Dispose();
+            }
+
+            await semaphore.WaitAsync();
+            try
+            {
+                await Task.Delay(1);
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+
+            return Task.CompletedTask;
+        });
+
+        await Task.WhenAll(tasks);
+    }
+
+    [Benchmark]
+    public async Task KeyedSemaphores()
+    {
+        var tasks = Enumerable.Range(0, Contention * NumberOfLocks).Select(async i =>
+        {
+            string key = i.ToString();
+
+            using var _ = await KeyedSemaphore.LockAsync(key);
+            
+            await Task.Delay(1);
+        });
+
+        await Task.WhenAll(tasks);
+    }
+}
--- KeyedSemaphores/AssemblyInfo.cs
+++ KeyedSemaphores/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("KeyedSemaphores.Tests")]
+[assembly: InternalsVisibleTo("KeyedSemaphores.Benchmarks")]
