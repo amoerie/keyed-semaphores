df1c872050b5ddf58141a47c9dd91e107c0daf51
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-10-23 11:52:44 -0400

    feat(Timeout): Add proper exception when the lock timeout

--- KeyedSemaphores.Tests/TestForCancellationTokenAndTimeout.cs
+++ KeyedSemaphores.Tests/TestForCancellationTokenAndTimeout.cs
@@ -24,4 +24,50 @@ public class TestForCancellationTokenAndTimeout
 
         collection.Index.Should().NotContainKey("test");
     }
+
+    [Fact]
+    public async Task TestTimeoutException()
+    {
+        var collection = new KeyedSemaphoresCollection<string>();
+
+        using var cts = new CancellationTokenSource();
+
+        var task = Task.Run(async () =>
+        {
+            using var _ = await collection.LockAsync("test", default, cts.Token);
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(10), cts.Token);
+            }
+        }, cts.Token);
+        var action = async () =>
+        {
+            using var locking = await collection.LockAsync("test", TimeSpan.FromMilliseconds(10), cts.Token);
+        };
+
+        await action.Should().ThrowAsync<TimeoutException>();
+
+        cts.Cancel();
+
+        collection.Index.Should().NotContainKey("test");
+    }
+    
+    [Fact]
+    public async Task TestNoTimeoutException()
+    {
+        var collection = new KeyedSemaphoresCollection<string>();
+
+        using var cts = new CancellationTokenSource();
+
+        var action = async () =>
+        {
+            using var locking = await collection.LockAsync("test", TimeSpan.FromMilliseconds(10), cts.Token);
+        };
+
+        await action.Should().NotThrowAsync<TimeoutException>();
+
+        cts.Cancel();
+
+        collection.Index.Should().NotContainKey("test");
+    }
 }
